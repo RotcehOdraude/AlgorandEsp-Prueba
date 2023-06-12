53907e3ec0458055e7779aad4931ba948b9fe658
RotcehOdraude <hectoreduardo354@hotmail.com>
2023-06-12 15:28:02 -0600

    Agregando funciones para el primer y segundo archivo y utilizando sus funciones en el ejemplo del repositorio de github

--- algorandEjemploAldeco/primero_crearCuenta.py
+++ algorandEjemploAldeco/primero_crearCuenta.py
@@ -5,7 +5,7 @@ def generar_cuenta_llavePrivada():
     Genera una nueva llave privada y la dirección de cuenta asociada.
 
     Returns:
-        Tuple: Una tupla que contiene la llave privada generada y la dirección de cuenta.
+        Tuple: Una tupla que contiene la llave privada generada, la dirección de cuenta y el mnemónico de la llave privada.
     
     Raises:
         None.
@@ -22,4 +22,25 @@ def generar_cuenta_llavePrivada():
     print("Private key mnemonic: " + mnemonic)
     print("Account address: " + account_address)
     
-    return private_key, account_address
+    return private_key, account_address, mnemonic
+
+def leer_cuentas_deArchivo(archivo):
+    """
+    Lee un archivo y procesa cada línea para obtener un diccionario de cuentas numeradas.
+
+    Parámetros:
+    - archivo (str): El nombre del archivo a leer.
+
+    Retorna:
+    - dict: Un diccionario donde sus elementos son las cuentas generadas y almacenadas en el archivo [archivo].txt,
+             y los valores son tuplas que contienen la clave y la dirección leídas de cada cuenta.
+
+    """
+    cuentas = {}
+    with open(archivo, "r") as file:
+        for i,linea in enumerate(file):
+            linea = linea.strip()  # Eliminar espacios en blanco al inicio y final de la línea
+            if linea:
+                clave, direccion = linea.split(",")  # Dividir la línea en clave y dirección
+                cuentas[i] = (clave, direccion)
+    return cuentas
--- proyecto.py
+++ proyecto.py
@@ -1,3 +1,54 @@
-from algorandEjemploAldeco import primero_crearCuenta
+from algorandEjemploAldeco.primero_crearCuenta import * 
+from algorandEjemploAldeco.segundo_first_transaction_example import *
 
-primero_crearCuenta.generar_cuenta_llavePrivada()
+### 1.1 CREANDO CUENTAS EN ALGORAND ###
+'''
+# Generando llave privada de cuenta A y su dirección
+llave_privada_A, direccion_cuenta_A, _ = generar_cuenta_llavePrivada()
+
+# Almacenando en un archivo de texto la llave privada y la dirección de la cuenta A
+with open("cuentas.txt", "w") as archivo:
+    archivo.write(f"A: {llave_privada_A},{direccion_cuenta_A}\n")
+
+# Generando llave privada de cuenta A y su dirección
+llave_privada_B, direccion_cuenta_B, _ = generar_cuenta_llavePrivada()
+
+# Almacenando en un archivo de texto la llave privada y la dirección de la cuenta A
+with open("cuentas.txt", "a") as archivo:
+    archivo.write(f"B: {llave_privada_B},{direccion_cuenta_B}\n")
+'''
+cuentas_creadas = leer_cuentas_deArchivo("cuentas.txt")
+llave_privada_A,direccion_cuenta_A = cuentas_creadas[0]
+llave_privada_B,direccion_cuenta_B = cuentas_creadas[1]
+
+### 1.2 AÑADIENDO FONDOS A LAS CUENTAS ###
+# URL: https://testnet.algoexplorer.io/dispenser
+
+### 1.3 VERIFICANDO EL BALANCE DE NUESTRAS CUENTAS ###
+# URL: https://testnet.algoexplorer.io/
+
+### 2. TU PRIMERA TRANSACCIÓN ###
+# Cualquier transacción puede incluir una "nota" arbitraria de hasta 1kb. En otras palabras, las notas permiten almacenar una pequeña cantidad de datos en la cadena de bloques que nos permitirá identificar una transacción de otra de manera local.
+
+### 2.1 CONECTA CON EL CLIENTE ###
+algod_client = conexion_con_cliente_algod(red="algonode")
+
+### 2.2 REVISAR EL SALDO DE LA CUENTA A ###
+saldo, account_info_A = verficar_balance_cuenta(algod_client, direccion_cuenta_A)
+
+### 2.3 CREAR UNA TRANSACCIÓN ###
+amount = 500000
+unsigned_txn,params = crear_transaccion(algod_client, direccion_cuenta_A, direccion_cuenta_B, amount, note="Esta es la primera transaccion de A")
+
+### 2.4 FIRMAR LA TRANSACCIÓN ###
+signed_txn = firmar_transaccion(unsigned_txn, llave_privada_A)
+
+### 2.5 ENVIAR UNA TRANSACCIÓN ###
+confirmed_txn,tx_id = enviar_transaccion(algod_client, signed_txn)
+
+# Imprimiendo información de la transacción
+imprimir_transaccion(algod_client,direccion_cuenta_A,account_info_A,confirmed_txn,amount,params)
+
+# Confirmando transaccion en el explorador
+# URL: https://testnet.algoexplorer.io/
+# Puedes usar el 
